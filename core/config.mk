bc97a4e0f25b5f9172fc316f92b763d9477007ae
dianlujitao <dianlujitao@lineageos.org>
2019-09-05 20:39:07 +0800

    core: Include our vendor config
    Change-Id: I4296948ca9e04a0025881bc702142df02727da70

--- core/config.mk
+++ core/config.mk
@@ -1278,4 +1278,6 @@ endif
 DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
 
+include vendor/proton/build/core/config.mk
+
 include $(BUILD_SYSTEM)/dumpvar.mk
