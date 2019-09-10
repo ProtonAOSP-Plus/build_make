9ee75fb763c504afc81591bb1e9e35813d0bcb55
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-09 22:21:19 -0300

    build: Add others targets on lunch menu [1/2]
    Change-Id: Idb5ed98634de1d6d2c8dade0811578c1d2a5f944

--- envsetup.sh
+++ envsetup.sh
@@ -606,8 +606,6 @@ function add_lunch_combo()
 function print_lunch_menu()
 {
     local uname=$(uname)
-    local choices
-    choices=$(TARGET_BUILD_APPS= TARGET_PRODUCT= TARGET_BUILD_VARIANT= get_build_var COMMON_LUNCH_CHOICES 2>/dev/null)
     local ret=$?
 
     echo
@@ -629,7 +627,7 @@ function print_lunch_menu()
 
     local i=1
     local choice
-    for choice in $(echo $choices)
+    for choice in ${choices[@]}
     do
         echo "     $i. $choice"
         i=$(($i+1))
@@ -641,6 +639,19 @@ function print_lunch_menu()
 function lunch()
 {
     local answer
+
+    choices=()
+    for makefile_target in $(TARGET_BUILD_APPS= TARGET_PRODUCT= TARGET_BUILD_VARIANT= get_build_var COMMON_LUNCH_CHOICES 2>/dev/null)
+    do
+        choices+=($makefile_target)
+    done
+    for other_target in ${lunch_others_targets[@]}
+    do
+        if [[ " ${choices[*]} " != *"$other_target"* ]];
+        then
+            choices+=($other_target)
+        fi
+    done
 
     if [[ $# -gt 1 ]]; then
         echo "usage: lunch [target]" >&2
@@ -662,7 +673,6 @@ function lunch()
         selection=aosp_arm-eng
     elif (echo -n $answer | grep -q -e "^[0-9][0-9]*$")
     then
-        local choices=($(TARGET_BUILD_APPS= get_build_var COMMON_LUNCH_CHOICES))
         if [ $answer -le ${#choices[@]} ]
         then
             # array in zsh starts from 1 instead of 0.
