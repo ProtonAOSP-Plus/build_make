f856ca81a25b4518a01ffc7bc36729daa8899b46
Md Abu Talha <rokusenpai06@gmail.com>
2022-03-16 12:50:13 +0000

    core: include our custom sepolicy
    Change-Id: I3b4bd07d4e42d9ac2b47573db71d21cee627c9aa

--- core/config.mk
+++ core/config.mk
@@ -1168,6 +1168,8 @@ dont_bother_goals := out \
 # consistency with those defined in BoardConfig.mk files.
 include $(BUILD_SYSTEM)/android_soong_config_vars.mk
 
+include device/proton/sepolicy/common/sepolicy.mk
+
 ifeq ($(CALLED_FROM_SETUP),true)
 include $(BUILD_SYSTEM)/ninja_config.mk
 include $(BUILD_SYSTEM)/soong_config.mk
